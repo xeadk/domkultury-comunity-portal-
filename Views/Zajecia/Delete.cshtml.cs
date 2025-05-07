5e6e569cf8e73d1b16e7a88d151d074473945ee7
xeadk <eadkv2@gmail.com>
2025-05-08 00:08:54 +0200

    CRUD zajecia oraz przylanie plikow html file

---
+++ Views/Zajecia/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DomKultury.Views.Zajecia
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
