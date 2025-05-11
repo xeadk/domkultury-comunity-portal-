53e6b4197499b283ae73a43f5a14ca3e9a4c96b9
gisztorowiczz <zuzannagisztorowicz@gmail.com>
2025-05-11 22:35:49 +0200

    dodanie filtrowania wydarzeń po kategorii, podświetlenie zakładki w której jesteśmy i cień i podniesienie karty po najechaniu na kartę

--- Controllers/WydarzeniaController.cs
+++ Controllers/WydarzeniaController.cs
@@ -2,6 +2,7 @@
 using DomKultury.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DomKultury.Controllers
@@ -16,12 +17,26 @@ namespace DomKultury.Controllers
         }
 
         // GET: /Wydarzenia
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? categoryId)
         {
-            var wydarzenia = await _context.Wydarzenie
+            // Tworzymy zapytanie do bazy, aby uwzględnić wydarzenia oraz kategorię
+            var wydarzeniaQuery = _context.Wydarzenie
                 .Include(w => w.Kategoria)
-                .ToListAsync();
+                .AsQueryable(); // Używamy AsQueryable(), aby móc dynamicznie dodawać warunki
 
+            // Filtrowanie po kategorii, jeśli id kategorii jest podane
+            if (categoryId.HasValue)
+            {
+                wydarzeniaQuery = wydarzeniaQuery.Where(w => w.KategoriaId == categoryId.Value);
+            }
+
+            // Pobranie wydarzeń, które spełniają warunki
+            var wydarzenia = await wydarzeniaQuery.ToListAsync();
+
+            // Pobranie listy kategorii do wyświetlenia w dropdownie
+            ViewBag.Kategorie = await _context.Kategoria.ToListAsync();
+
+            // Przekazanie listy wydarzeń i kategorii do widoku
             return View(wydarzenia);
         }
     }
