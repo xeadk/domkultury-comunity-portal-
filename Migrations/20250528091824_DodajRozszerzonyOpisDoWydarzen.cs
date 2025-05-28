a20e69008df8a30253383109501fce165dca28ed
gisztorowiczz <zuzannagisztorowicz@gmail.com>
2025-05-28 11:31:36 +0200

    dodanie rozszerzonego opisu do szczegółów zajęć i wydarzeń

---
+++ Migrations/20250528091824_DodajRozszerzonyOpisDoWydarzen.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DomKultury.Migrations
+{
+    /// <inheritdoc />
+    public partial class DodajRozszerzonyOpisDoWydarzen : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RozszerzonyOpis",
+                table: "Wydarzenie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RozszerzonyOpis",
+                table: "Wydarzenie");
+        }
+    }
+}
