a4e1991d2f30c3b221e3e4aec3a7bde684a40e8c
Adask3s <adam.kopystecki@gmail.com>
2025-06-04 00:19:01 +0200

    zmiana na Warszawa

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -59,7 +59,7 @@ namespace DomKultury.Controllers
                 _logger.LogWarning("Brak klucza API do pobierania pogody.");
             }
 
-            var pogoda = await PobierzPogodeAsync("Bia³ystok", apiKey); // lub inne miasto
+            var pogoda = await PobierzPogodeAsync("Warszawa", apiKey); // lub inne miasto
 
             var model = new HomePageViewModel
             {
